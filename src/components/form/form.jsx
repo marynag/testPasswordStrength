d5ed3a3ef50c2afd6cc581392dbf9222f0f991a5
marynag <mgrabchak1109@gmail.com>
2023-02-24 15:05:04 +0200

    add password cheking

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,27 +1,50 @@
 import styles from './forms.module.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const Form = () => {
 	const [userInput, setUserInput] = useState('');
+	const [status, setStatus] = useState('');
 
-	const handleChange = (event) => {
-		setUserInput(event.target.value);
-	};
+	useEffect(() => {
+		const containLetters = !!userInput.match(/(.*[A-z].*)/);
+		const containNumber = !!userInput.match(/(.*[0-9].*)/);
+		const containSpecialChar = !!userInput.match(/(.*[-+_!@#$%^&*.,?].*)/);
+		if (containLetters || containNumber || containSpecialChar) {
+			setStatus('weak');
+		}
+		if (
+			(containLetters && containSpecialChar) ||
+			(containLetters && containNumber) ||
+			(containSpecialChar && containNumber)
+		) {
+			setStatus('medium');
+		}
+		if (containLetters && containNumber && containSpecialChar) {
+			setStatus('strong');
+		}
+		if (userInput.length < 8) {
+			setStatus('short');
+		}
+		if (!userInput) {
+			setStatus('');
+		}
+	}, [userInput]);
 
 	return (
 		<form className={styles.form}>
-			<h2>Password Validator</h2>
+			<h2>Test Password Strength</h2>
 			<input
-				onChange={handleChange}
+				onChange={(e) => setUserInput(e.target.value)}
 				value={userInput}
 				placeholder='Enter password'
 				type='password'
 			/>
 			<div className={[`${styles.passwordCheacking}`]}>
-				<div></div>
-				<div></div>
-				<div></div>
+				<div className={[`${styles[status]}`]}></div>
+				<div className={[`${styles[status]}`]}></div>
+				<div className={[`${styles[status]}`]}></div>
 			</div>
+			{status && <p>Your password is {status}</p>}
 		</form>
 	);
 };
